94f63d20be8eb3b8f97fcbf170b2d86d44025ecc
nnnewb <weak_ptr@outlook.com>
2022-12-04 10:18:47 +0000

    refactor: configurable log dir and more

--- main.go
+++ main.go
@@ -1,104 +1,180 @@
 package main
 
 import (
+	"context"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
+	"encoding/hex"
 	"encoding/pem"
-	"flag"
+	"errors"
+	"fmt"
 	"io"
-	"log"
 	"net"
 	"os"
 	"os/signal"
-	"runtime"
+	"path"
 	"strings"
 	"time"
 
+	"github.com/spf13/pflag"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"golang.org/x/crypto/ssh"
 	terminal "golang.org/x/term"
 )
 
-var bindAddressFlag = flag.String("b", "0.0.0.0", "Bind Address")
-var portFlag = flag.String("p", "22", "Port Number")
+type CliArgument struct {
+	Addr    string
+	Debug   bool
+	LogDir  string
+	PrivKey string
+}
+
+var cliArgument CliArgument
+var logger *zap.Logger
+
+func init() {
+	pflag.StringVarP(&cliArgument.Addr, "address", "a", "0.0.0.0:2222", "Bind address")
+	pflag.BoolVarP(&cliArgument.Debug, "debug", "d", false, "toggle debug mode")
+	pflag.StringVarP(&cliArgument.LogDir, "log", "l", "./", "Log output dir.")
+	pflag.StringVarP(&cliArgument.PrivKey, "privatekey", "p", "priv.pem", "SSH private key")
+	pflag.Usage = func() {
+		fmt.Printf("|---------------------------------------------------------------|\n")
+		fmt.Printf("|                                                               |\n")
+		fmt.Printf("|             SSH Honeypot written in Go                        |\n")
+		fmt.Printf("|                                                               |\n")
+		fmt.Printf("| This SSH daemon will accept any username/password/key.        |\n")
+		fmt.Printf("| It only allows 'session' channels (not port forwards or SFTP).|\n")
+		fmt.Printf("| It will present a fake shell and record any commands that     |\n")
+		fmt.Printf("| people attempt to run, along with the date and their IP.      |\n")
+		fmt.Printf("|                                                               |\n")
+		fmt.Printf("| Usage:                                                        |\n")
+		fmt.Printf("|   %s [options]                                                |\n", os.Args[0])
+		fmt.Printf("|                                                               |\n")
+		fmt.Printf("|---------------------------------------------------------------|\n")
+		pflag.PrintDefaults()
+		os.Exit(0)
+	}
+}
+
+func setupLogger() error {
+	file, err := os.OpenFile(path.Join(cliArgument.LogDir, "sshhighpot.log"), os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0o600)
+	if err != nil {
+		return err
+	}
+
+	if cliArgument.Debug {
+		enc := zap.NewDevelopmentEncoderConfig()
+		enc.EncodeTime = func(t time.Time, pae zapcore.PrimitiveArrayEncoder) {
+			pae.AppendString(t.Format(time.RFC3339))
+		}
+
+		logger = zap.New(
+			zapcore.NewTee(
+				zapcore.NewCore(zapcore.NewConsoleEncoder(enc), zapcore.Lock(os.Stderr), zap.InfoLevel),
+				zapcore.NewCore(zapcore.NewJSONEncoder(enc), zapcore.Lock(file), zap.InfoLevel),
+			),
+			zap.AddCaller(),
+			zap.AddStacktrace(zap.ErrorLevel),
+		)
+	} else {
+		enc := zap.NewProductionEncoderConfig()
+		enc.EncodeTime = func(t time.Time, pae zapcore.PrimitiveArrayEncoder) {
+			pae.AppendString(t.Format(time.RFC3339))
+		}
+
+		logger = zap.New(
+			zapcore.NewTee(
+				zapcore.NewCore(zapcore.NewConsoleEncoder(enc), zapcore.Lock(os.Stderr), zap.InfoLevel),
+				zapcore.NewCore(zapcore.NewJSONEncoder(enc), zapcore.Lock(file), zap.InfoLevel),
+			),
+			zap.AddCaller(),
+			zap.AddStacktrace(zap.ErrorLevel),
+		)
+	}
+
+	return nil
+}
+
+// loadPrivKey load private key from file, if file not exists,
+// generate one.
+func loadPrivKey(privKey string) ssh.Signer {
+	var privatekey []byte
+	if file, err := os.OpenFile(cliArgument.PrivKey, os.O_RDONLY, 0o600); err != nil {
+		if os.IsNotExist(err) {
+			logger.Info("Private key not exists, generate now", zap.String("privatekey", cliArgument.PrivKey))
+			privatekey = generateHostKey(1024)
+			if file, err := os.OpenFile(cliArgument.PrivKey, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600); err != nil {
+				logger.Fatal("open private key for write failed", zap.String("filepath", cliArgument.PrivKey), zap.Error(err))
+			} else {
+				defer file.Close()
+				file.Write(privatekey)
+			}
+		} else {
+			logger.Fatal("open private key for read failed", zap.String("filepath", cliArgument.PrivKey), zap.Error(err))
+		}
+	} else {
+		var err error
+		privatekey, err = io.ReadAll(file)
+		if err != nil {
+			logger.Fatal("read private key failed", zap.String("filepath", cliArgument.PrivKey), zap.Error(err))
+		}
+	}
+
+	logger.Info("Private key loaded from file", zap.String("privatekey", cliArgument.PrivKey))
+	hostKey, err := ssh.ParsePrivateKey(privatekey)
+	if err != nil {
+		logger.Fatal("Error: Failed to parse host key", zap.Error(err))
+	}
+
+	return hostKey
+}
 
 func main() {
-
-	log.Print("\n")
-	log.Print("***************************************************************\n")
-	log.Print("                                                               \n")
-	log.Print("              gohoney: SSH Honeypot written in Go              \n")
-	log.Print("                                                               \n")
-	log.Print(" This SSH daemon will accept any username/password/key.        \n")
-	log.Print(" It only allows 'session' channels (not port forwards or SFTP).\n")
-	log.Print(" It will present a fake shell and record any commands that     \n")
-	log.Print(" people attempt to run, along with the date and their IP.      \n")
-	log.Print("                                                               \n")
-	log.Print(" It will log all sessions to:                                  \n")
-	log.Print(" /var/log/gohoney-YYYYMMDD.log                                 \n")
-	log.Print("                                                               \n")
-	log.Print(" Usage:                                                        \n")
-	log.Print(" ./gohoney -b <bind address> -p <port>                         \n")
-	log.Print("                                                               \n")
-	log.Print("***************************************************************\n")
-
-	flag.Parse()
-
-	// Set the bind address for the server
-	bind := *bindAddressFlag + ":" + *portFlag
-
-	log.Printf("Settings max processes to %d", runtime.NumCPU())
-	runtime.GOMAXPROCS(runtime.NumCPU())
+	pflag.Parse()
+	setupLogger()
 
 	// An SSH server is represented by a ServerConfig, which holds
 	// certificate details and handles authentication of ServerConns.
-
 	config := &ssh.ServerConfig{
-		PasswordCallback: func(c ssh.ConnMetadata, pass []byte) (*ssh.Permissions, error) {
-			// Accept any username/password
-			log.Printf("Accepted user authentication (%s/%s) from %s", c.User(), string(pass), c.RemoteAddr().String())
-			return &ssh.Permissions{}, nil
-		},
-		PublicKeyCallback: func(c ssh.ConnMetadata, pubKey ssh.PublicKey) (*ssh.Permissions, error) {
-			// Accept any private key
-			log.Printf("Accepted key authentication for user %s from %s", c.User(), c.RemoteAddr().String())
-			return &ssh.Permissions{}, nil
-		},
+		PasswordCallback:            passwordCallback,
+		PublicKeyCallback:           publicKeyCallback,
+		KeyboardInteractiveCallback: keyboardInteractiveCallback,
 	}
 
 	// Setup a host key to use
-	keyBytes := generateHostKey(1024)
-
-	hostKey, err := ssh.ParsePrivateKey(keyBytes)
-	if err != nil {
-		log.Fatalf("Error: Failed to parse host key (%s)", err)
-	}
-
-	config.AddHostKey(hostKey)
+	config.AddHostKey(loadPrivKey(cliArgument.PrivKey))
 
 	// Now that we've configured the server, we can start listening
-	socket, err := net.Listen("tcp", bind)
+	listener, err := net.Listen("tcp", cliArgument.Addr)
 	if err != nil {
-		log.Fatalf("Error: Failed to bind to %s (%s)", bind, err)
+		logger.Fatal("Error: Failed to bind address", zap.String("address", cliArgument.Addr), zap.Error(err))
 	}
 
-	log.Printf("Listening on %s", bind)
+	logger.Sugar().Infof("Listening on %s", cliArgument.Addr)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
 	go func() {
 		for {
 			// A ServerConn multiplexes several channels, which must
 			// themselves be Accepted.
-			networkConnection, err := socket.Accept()
-
+			networkConnection, err := listener.Accept()
 			if err != nil {
-				log.Printf("Error: Failed to accept an incoming connection from %s (%s)", socket.Addr().String(), err)
-				continue
+				select {
+				case <-ctx.Done():
+					return
+				default:
+					logger.Sugar().Infof("Error: Failed to accept an incoming connection from %s (%s)", listener.Addr().String(), err)
+					continue
+				}
 			}
 
 			// Launch a new goroutine (lite-thread) to handle the connection
 			// freeing us up to accept more.
 			go handleNetworkConnection(networkConnection, config)
-
 		}
 	}()
 
@@ -107,23 +183,40 @@ func main() {
 
 	<-c
 
-	log.Println("Received a ctrl+c - shutting down...")
-	if socket != nil {
-		socket.Close()
+	logger.Info("Received a ctrl+c - shutting down...")
+	cancel()
+	if listener != nil {
+		listener.Close()
 	}
 }
 
-func handleNetworkConnection(networkConnection net.Conn, config *ssh.ServerConfig) {
+func passwordCallback(conn ssh.ConnMetadata, password []byte) (*ssh.Permissions, error) {
+	// Accept any username/password
+	logger.Sugar().Infof("Accepted user authentication (%s/%s) from %s", conn.User(), string(password), conn.RemoteAddr().String())
+	return &ssh.Permissions{}, nil
+}
 
+func publicKeyCallback(conn ssh.ConnMetadata, pubKey ssh.PublicKey) (*ssh.Permissions, error) {
+	// Accept any private key
+	logger.Sugar().Infof("Accepted key authentication for user %s from %s", conn.User(), conn.RemoteAddr().String())
+	return &ssh.Permissions{}, nil
+}
+
+func keyboardInteractiveCallback(conn ssh.ConnMetadata, client ssh.KeyboardInteractiveChallenge) (*ssh.Permissions, error) {
+	// Accept any private key
+	logger.Sugar().Infof("Accepted keyboard interactive authentication for user %s from %s", conn.User(), conn.RemoteAddr().String())
+	return &ssh.Permissions{}, nil
+}
+
+func handleNetworkConnection(networkConnection net.Conn, config *ssh.ServerConfig) {
 	serverConnection, newChan, _, err := ssh.NewServerConn(networkConnection, config)
 	if err != nil {
-		log.Printf("Error: SSH handshake failed (%s)", err)
+		logger.Sugar().Infof("Error: SSH handshake failed (%s)", err)
 		networkConnection.Close()
 		return
 	}
 
 	for {
-
 		// Accept reads from the connection, demultiplexes packets
 		// to their corresponding channels and returns when a new
 		// channel request is seen. Some goroutine must always be
@@ -131,26 +224,9 @@ func handleNetworkConnection(networkConnection net.Conn, config *ssh.ServerConfi
 		// to the channels.
 		channelRequest := <-newChan
 		if channelRequest == nil {
-			log.Printf("Connection closed: %s", networkConnection.RemoteAddr())
+			logger.Sugar().Infof("Connection closed: %s", networkConnection.RemoteAddr())
 			return
 		}
-		// channelRequest, err := serverConnection.Accept()
-
-		// if serverConnection == nil {
-		// 	return
-		// }
-
-		// if IsEOF(err) {
-		// 	log.Printf("Connection from %s closed", serverConnection.RemoteAddr())
-		// 	serverConnection.Close()
-		// 	serverConnection = nil
-		// 	return
-		// }
-
-		// if err != nil {
-		// 	log.Printf("Error: Failed to open channel (%s)", err)
-		// 	break
-		// }
 
 		// Channels have a type, depending on the application level
 		// protocol intended. In the case of a shell, the type is
@@ -160,35 +236,28 @@ func handleNetworkConnection(networkConnection net.Conn, config *ssh.ServerConfi
 		switch channelRequest.ChannelType() {
 		case "session":
 			go func() {
-				handleSessionChannel(channelRequest, serverConnection.User(), serverConnection.RemoteAddr().String())
-				log.Printf("Closing connection from %s", serverConnection.RemoteAddr())
+				handleSessionChannel(channelRequest, serverConnection)
+				logger.Sugar().Infof("Closing connection from %s", serverConnection.RemoteAddr())
 				serverConnection.Close()
 				serverConnection = nil
 			}()
 
 		default:
-			log.Printf("Error: Refusing to open unknown channel type: %s", channelRequest.ChannelType())
+			logger.Sugar().Infof("Error: Refusing to open unknown channel type: %s", channelRequest.ChannelType())
 			channelRequest.Reject(ssh.UnknownChannelType, "unknown channel type")
 		}
-
 	}
 }
 
-func handleSessionChannel(channelRequest ssh.NewChannel, user, addr string) {
-
+func handleSessionChannel(channelRequest ssh.NewChannel, conn *ssh.ServerConn) {
 	sessionChannel, _, err := channelRequest.Accept()
 	if err != nil {
-		log.Printf("Could not accept direct-tcpip channel from %s", channelRequest)
+		logger.Sugar().Infof("Could not accept direct-tcpip channel from %s", channelRequest)
 		return
 	}
-
 	defer sessionChannel.Close()
 
-	term := terminal.NewTerminal(sessionChannel, user+"@server35:~$ ")
-	// serverTerm := &terminal.Terminal{
-	// 	Term:    term,
-	// 	Channel: sessionChannel,
-	// }
+	term := terminal.NewTerminal(sessionChannel, conn.User()+"@server35:~$ ")
 
 	// Generate the date for the banner
 	// Format: Thu Dec 31 15:30:14 GMT 2013
@@ -216,61 +285,63 @@ func handleSessionChannel(channelRequest ssh.NewChannel, user, addr string) {
 	term.Write([]byte("Last login: Wed Aug 23 18:28:57 2013 from 10.10.35.1\r\n"))
 	term.Write([]byte("\r\n"))
 
-	logDate := time.Now().Format("20060201")
-	logFileName := "/var/log/gohoney-" + logDate + ".log"
-	logFile, err := os.OpenFile(logFileName, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
+	logFile, err := os.OpenFile(path.Join(cliArgument.LogDir, fmt.Sprintf("ssh-session-%s-%s.log", time.Now().Format("20060102"), hex.EncodeToString(conn.SessionID()))), os.O_CREATE|os.O_WRONLY, 0600)
 	if err != nil {
-		log.Printf("Error: Could not write to %s (%s)", logFileName, err)
+		logger.Error("open session log file failed", zap.Error(err))
+		return
 	}
 
-	logFile.WriteString("***************************************************************\r\n")
-	logFile.WriteString(" Session Started: " + date + "\r\n")
-	logFile.WriteString(" From: " + addr + "\r\n")
+	sessionLogger := zap.New(
+		zapcore.NewTee(
+			zapcore.NewCore(zapcore.NewConsoleEncoder(zap.NewProductionEncoderConfig()), zapcore.Lock(os.Stderr), zap.DebugLevel),
+			zapcore.NewCore(zapcore.NewConsoleEncoder(zap.NewProductionEncoderConfig()), zapcore.Lock(logFile), zap.DebugLevel),
+		),
+		zap.AddCaller(),
+		zap.AddStacktrace(zap.ErrorLevel),
+		zap.Fields(
+			zap.Any("ssh", map[string]interface{}{
+				"remote_addr":    conn.RemoteAddr().String(),
+				"session_id":     hex.EncodeToString(conn.SessionID()),
+				"client_version": fmt.Sprintf("%q", conn.ClientVersion()),
+				"server_version": fmt.Sprintf("%q", conn.ServerVersion()),
+				"user":           conn.User(),
+			})),
+	)
+
+	sessionLogger.Info("session started", zap.Time("time", time.Now()))
+	sessionLogger.Info("peer address", zap.String("peer", conn.RemoteAddr().String()))
 
 	defer func() {
-		finishedDate := time.Now().Format("Mon Jan 2 15:04:05 MST 2006")
-		logFile.WriteString(" Session Finished: " + finishedDate + "\r\n")
-		logFile.WriteString("***************************************************************\r\n\r\n")
-		logFile.Close()
+		sessionLogger.Info("session finished", zap.Time("time", time.Now()))
+		sessionLogger.Sync()
 	}()
 
 	for {
-
 		line, err := term.ReadLine()
-
-		if IsEOF(err) {
-			return
-		}
 		if err != nil {
-			log.Println("Could not read command:", err)
-			continue
+			if errors.Is(err, io.EOF) {
+				return
+			}
+			sessionLogger.Error("Read command failed", zap.Error(err))
 		}
 
-		entryTime := time.Now().Format("15:04:05")
-		entry := " " + entryTime + ": " + line + "\r\n"
-		logFile.WriteString(entry)
-
-		log.Printf("%s entered: %s", addr, line)
-		if strings.Contains(line, "exit") {
+		sessionLogger.Info("Record command line input", zap.String("command", line))
+		if strings.TrimSpace(line) == "exit" {
 			return
 		}
-
 	}
 }
 
-/**
- * Generates a private key and returns it as a byte array
- */
+// generateHostKey Generates a private key and returns it as a byte array
 func generateHostKey(bits int) (key []byte) {
-
 	privateKey, err := rsa.GenerateKey(rand.Reader, bits)
 	if err != nil {
-		log.Fatalf("Could not generate host key (%s)", err)
+		logger.Fatal("Could not generate host key", zap.Error(err))
 	}
 
 	privateKey.Validate()
 	if err != nil {
-		log.Fatalf("Could not validate host key (%s)", err)
+		logger.Fatal("Could not validate host key", zap.Error(err))
 	}
 
 	// Get der format. priv_der []byte
@@ -285,29 +356,4 @@ func generateHostKey(bits int) (key []byte) {
 	// Resultant private key in PEM format.
 	return pem.EncodeToMemory(&privateKeyBlock)
 
-}
-
-/**
- * A utility function which tests if an error returned from a TCPConnection or
- * TCPListener is actually an EOF. In some edge cases this which should be treated
- * as EOFs are not returned as one.
- */
-func IsEOF(err error) bool {
-	if err == nil {
-		return false
-	} else if err == io.EOF {
-		return true
-	} else if oerr, ok := err.(*net.OpError); ok {
-		/* this hack happens because the error is returned when the
-		 * network socket is closing and instead of returning a
-		 * io.EOF it returns this error.New(...) struct. */
-		if oerr.Err.Error() == "use of closed network connection" {
-			return true
-		}
-	} else {
-		if err.Error() == "use of closed network connection" {
-			return true
-		}
-	}
-	return false
 }
